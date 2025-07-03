7c28376f0a1cbb50dd9547a084915a401faaf100
hkv28032005 <hkvr28032005@gmail.com>
2025-07-03 20:41:45 +0530

    ems project

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js (ESM)
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+}
+
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10 bg-green-600 text-white text-4xl rounded-xl">
+      If you see this styled, Tailwind is working!
+    </div>
+  )
+}
+
+// export default App
